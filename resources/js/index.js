8ef18f6eb3299a24386fd19fc256eeb1ac3a7e82
Alecto1b <evansatria256@gmail.com>
2026-07-24 15:25:04 +0800

    minimize frontend upgrade changes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,46 +1,46 @@
-import ace from "ace-builds";
-import "ace-builds/src-noconflict/mode-ini";
+import ace from 'ace-builds'
+import 'ace-builds/src-noconflict/mode-ini'
 
-export default ({ maxLines, minLines, fontSize }) => ({
-  /** @type {ace.Ace.Editor} */
-  editor: null,
-  updateContentHandler: null,
+export default ({
+    maxLines,
+    minLines,
+    fontSize,
+}) => ({
+    /** @type {ace.Ace.Editor} */
+    editor: null,
+    updateContentHandler: null,
 
-  init() {
-    this.editor = ace.edit(this.$refs.editor, {
-      mode: "ace/mode/ini",
-      readOnly: true,
-      maxLines,
-      minLines,
-      fontSize,
-      showPrintMargin: false,
-    });
+    init() {
+        this.editor = ace.edit(this.$refs.editor, {
+            mode: 'ace/mode/ini',
+            readOnly: true,
+            maxLines,
+            minLines,
+            fontSize
+        });
 
-    this.updateContentHandler = (e) => {
-      this.editor.session.setValue(e.detail.content);
-    };
+        this.updateContentHandler = (e) => {
+            this.editor.session.setValue(e.detail.content)
+        }
 
-    window.addEventListener("logContentUpdated", this.updateContentHandler);
-  },
+        window.addEventListener('logContentUpdated', this.updateContentHandler)
+    },
 
-  destroy() {
-    if (this.updateContentHandler) {
-      window.removeEventListener(
-        "logContentUpdated",
-        this.updateContentHandler,
-      );
-      this.updateContentHandler = null;
+    destroy() {
+        if (this.updateContentHandler) {
+            window.removeEventListener('logContentUpdated', this.updateContentHandler)
+            this.updateContentHandler = null
+        }
+
+        this.editor?.destroy()
+        this.editor = null
+    },
+
+    jumpToEnd() {
+        this.editor.gotoLine(this.editor.session.doc.$lines.length)
+    },
+
+    jumpToStart() {
+        this.editor.gotoLine(0)
     }
-
-    this.editor?.destroy();
-    this.editor = null;
-  },
-
-  jumpToEnd() {
-    this.editor.gotoLine(this.editor.session.doc.$lines.length);
-  },
-
-  jumpToStart() {
-    this.editor.gotoLine(0);
-  },
-});
+})
